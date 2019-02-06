2e3bf409e293bf275d9b0a3485b3cba1f5fa5e3a
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-02-06 18:38:14 +0500

    Completed swipe-deck

--- App.js
+++ App.js
@@ -62,10 +62,23 @@ class App extends Component {
     );
   };
 
+  renderNoMoreCards() {
+    return (
+      <Card title="All Done!">
+        <Text style={{ marginBottom: 10 }}>There is no more content here!</Text>
+        <Button backgroundColor="#03A9F4" title="Get more!" />
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Deck data={DATA} renderCard={this.renderCard} />
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
       </View>
     );
   }
--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,27 @@
-//Whenever user presses on screen and drag around with finger we use PanResponder or 
-//gesture system. So three Questions: 
+//Whenever user presses on screen and drag around with finger we use PanResponder or
+//gesture system. So three Questions:
 //1-what are we touching?; 2-What comp handles touch?; 3-How is gesture changing
 import React, { Component } from "react";
-import { Animated, View, PanResponder } from "react-native";
+import {
+  Animated,
+  View,
+  PanResponder,
+  Dimensions,
+  LayoutAnimation,
+  UIManager
+} from "react-native";
+//Animated.View gets called only one time; it breaks the react conventions
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  };
+
   constructor(props) {
     super(props);
 
@@ -13,28 +30,107 @@ class Deck extends Component {
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        position.setValue({ x: gesture.dx, y: gesture.dy });
-      }, //Link between gesture system and animated system
-      onPanResponderRelease: () => {}
+        position.setValue({ x: gesture.dx, y: gesture.dy }); //updating with new values
+      }, //dx and dy—how far the user has moved the element
+      //Link between gesture system and animated system; previously handled by Animated.sprin
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe("right");
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe("left");
+        } else {
+          this.resetPosition();
+        }
+      }
     });
-    this.state = { panResponder, position };
+    this.state = { panResponder, position, index: 0 };
+    //panResponder is a self-contained object; we are not updating panResponder
+    //we could replace it with this.panResponder = {panResponder};
+  }
+
+  componentWillUpdate() {
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring(); //next time it re-renders, animate any changes
+  } // only for android
+
+  componentWillReceiveProps = nextProps => {
+    if (nextProps.data !== this.props.data) {
+      this.setState({ index: 0 });
+    }
+  };
+
+  forceSwipe(direction) {
+    const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ["-120deg", "0deg", "120deg"]
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
   }
 
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards();
+    }
+
+    return this.props.data
+      .map((item, i) => {
+        if (i < this.state.index) {
+          return null;
+        }
+
+        if (i === this.state.index) {
+          return (
+            <Animated.View
+              key={item.id}
+              style={[this.getCardStyle(), styles.cardStyle, { zIndex: 99 }]}
+              {...this.state.panResponder.panHandlers}
+            >
+              {this.props.renderCard(item)}
+            </Animated.View>
+          );
+        }
         return (
           <Animated.View
-          key={item.id}
-          style={this.state.position.getLayout()}
-          {...this.state.panResponder.panHandlers}
+            key={item.id}
+            style={[
+              styles.cardStyle,
+              { top: 10 * (i - this.state.index), zIndex: 5 }
+            ]}
           >
             {this.props.renderCard(item)}
           </Animated.View>
         );
-      }
-      return this.props.renderCard(item);
-    });
+      })
+      .reverse();
   }
 
   render() {
@@ -42,4 +138,14 @@ class Deck extends Component {
   }
 }
 
+const styles = {
+  cardStyle: {
+    position: "absolute",
+    width: SCREEN_WIDTH
+  } //when abs position is used View shrinks to abs min size required to be displayed
+};
+
 export default Deck;
+
+//Animated System and Gesture System are completely decoupled; they don't know whether
+// the other one exist
